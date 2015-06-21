f862e4179be2e11a649fe44514a68b526337c2b8
Anwar Baroudi <mabaroudi@berkeley.edu>
2015-06-21 10:49:18 -0700

    created a basic data model that supports adding, removing and simple text-to-speech

---
+++ DataModel.swift
@@ -0,0 +1,39 @@
+//
+//  DataModel.swift
+//  ImageSpeak
+//
+//  Created by Anwar Baroudi on 6/21/15.
+//  Copyright (c) 2015 Paige Plander. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class DataModel {
+    var imageData: [UIImage]
+    var labelData: [String]
+    let speaker: AVSpeechSynthesizer
+    
+    init() {
+        imageData = []
+        labelData = []
+        speaker = AVSpeechSynthesizer()
+    }
+    
+    func add(image: UIImage, label: String) {
+        imageData.append(image)
+        labelData.append(label)
+    }
+    
+    func remove(index: Int) {
+        imageData.removeAtIndex(index)
+        labelData.removeAtIndex(index)
+    }
+    
+    func speakAtIndex(index: Int) {
+        let speech = AVSpeechUtterance(string: labelData[index])
+        speaker.speakUtterance(speech)
+    }
+    
+}
