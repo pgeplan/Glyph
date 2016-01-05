2d6ebd82b57567e7d3bbf5b78811199f72bf161b
Anwar Baroudi <mabaroudi@berkeley.edu>
2016-01-04 21:22:01 -0600

    settings view was not actually hooked up from the storyboard to the swift file, (had to change the settingsviwcontrolller to be a class of type UITableViewController) so that is hooked up, switch now remembers its state, and the segues for the settings back to the main or basic are setup...but basic always crashes the app

--- Glyph/SettingsViewController.swift
+++ Glyph/SettingsViewController.swift
@@ -7,24 +7,44 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var settingsNavBar: UINavigationBar!
     @IBOutlet weak var settingsNavItem: UINavigationItem!
     @IBOutlet weak var settingsBackButton: UIBarButtonItem!
     var data = DataModel(isNewEmptyDataModel: false)
-    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
     @IBOutlet weak var basicModeSwitch: UISwitch!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let basicModeSwitchState = userDefaults.boolForKey("basicMode")
+        basicModeSwitch.setOn(basicModeSwitchState, animated: false)
+        basicModeSwitch.addTarget(self, action: Selector("stateChanged:"), forControlEvents: UIControlEvents.ValueChanged)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //basicModeSwitch action
+    func stateChanged(switchState: UISwitch) {
+        if basicModeSwitch.on {
+            userDefaults.setBool(true, forKey: "basicMode")
+        } else {
+            userDefaults.setBool(false, forKey: "basicMode")
+        }
+    }
+    
+    @IBAction func backButton(sender: UIButton) {
+        if basicModeSwitch.on {
+            performSegueWithIdentifier("settingsToBasic", sender: self)
+        } else {
+            performSegueWithIdentifier("settingsToMain", sender: self)
+        }
     }
     
     //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
