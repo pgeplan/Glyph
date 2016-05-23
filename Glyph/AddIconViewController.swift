97a8f9eaf6522f79fe5f94f686c0c60a0a14ac16
Paige Plander <planderpaige@gmail.com>
2016-05-22 18:58:57 -0700

    Putting DataModel and AddIconViewController back in right spot

---
+++ Glyph/AddIconViewController.swift
@@ -0,0 +1,216 @@
+//
+//  SettingsModalViewController.swift
+//  Created by Paige Plander on 4/21/16.
+//  Copyright (c) 2016 Paige Plander. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class AddIconViewController: UITableViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    var data = DataModel(isNewEmptyDataModel: false)
+    
+    // delete when we get folders working
+    var folders: [String] = NSUserDefaults.standardUserDefaults().arrayForKey("folders") as? [String] ?? ["General"]
+    
+    
+    @IBOutlet weak var folderTextField: UITextField!
+    // Image View attribute
+    @IBOutlet var imagePicker: UIImageView!
+    
+    @IBOutlet weak var previewLabel: UILabel!
+    
+    var img: UIImage?
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    let picker = UIImagePickerController()
+    
+    // Text Field for User to add Picture Label
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func shootPhoto(sender: UIButton) {
+        if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
+            picker.delegate = self
+            picker.allowsEditing = false
+            picker.sourceType = .Camera
+            picker.cameraCaptureMode = .Photo
+            presentViewController(picker, animated: true, completion: nil)
+            
+        } else {
+            noCamera()
+        }
+    }
+    
+    
+    func returnImage() -> UIImage {
+        return imagePicker.image!
+    }
+    
+    func returnImageName() -> String? {
+        if textField.text == "" {
+            return nil
+        }
+        return textField.text
+    }
+    
+    func returnFolderName() -> String {
+        if let folderName = folderTextField.text {
+            if folderName == "" {
+                return "General"
+            }
+            else {
+                return folderName
+            }
+        }
+        return "General"
+    }
+    
+    // Alert if device doesn't have camera button
+    func noCamera(){
+        let alertVC = UIAlertController(title: "Camera Feature Not Available", message: "This device does not have a camera", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style:.Default, handler: nil)
+        alertVC.addAction(okAction)
+        presentViewController(alertVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func textEdited(sender: UITextField) {
+        previewLabel.text = sender.text
+    }
+    
+    @IBAction func chooseFromLibrary(sender: UIButton) {
+        picker.allowsEditing = false
+        picker.sourceType = .PhotoLibrary
+        picker.modalPresentationStyle = .Popover
+        presentViewController(picker, animated: true, completion: nil)
+        picker.popoverPresentationController?.sourceView = sender
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        picker.delegate = self
+        let folderPicker = UIPickerView()
+        folderPicker.dataSource = self
+        folderPicker.delegate = self
+        self.folderTextField.inputView = folderPicker
+    }
+    
+    @IBAction func resign() {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func notifyUserOfError(popUpTitle: String, popUpMessage: String, popUpButtonLabel: String) -> Void {
+        let alertController = UIAlertController(title: popUpTitle, message: popUpMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: popUpButtonLabel, style: UIAlertActionStyle.Default,handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // taken from http://stackoverflow.com/questions/27833075/swift-uilabel-programmatically-updates-after-uibutton-pressed
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        let pickedImage: UIImage = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
+        let sizeOfImageView:CGRect = imagePicker.frame
+        imagePicker.frame = sizeOfImageView
+        imagePicker.image = pickedImage
+        img = imagePicker.image
+        
+    }
+    
+    
+    // scale down the image size for a tile
+    func scaleDownImage(image: UIImage) -> UIImage {
+        let size = image.size
+        let widthRatio  = 150/image.size.width
+        let heightRatio = 150/image.size.height
+        var newImageSize: CGSize
+        if(widthRatio > heightRatio) {
+            newImageSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
+        } else {
+            newImageSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio)
+        }
+        let rect = CGRectMake(0, 0, 150, 150)
+        UIGraphicsBeginImageContextWithOptions(newImageSize, false, 1.0)
+        image.drawInRect(rect)
+        let imageToReturn = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return imageToReturn
+    }
+    
+    
+    // Executes if the user wants to cancel (inside choose Photo)
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func doneButton(sender: UIButton) {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let basic = userDefaults.boolForKey("basicMode")
+        //identifier doesn't matter here
+        if shouldPerformSegueWithIdentifier("", sender: self) {
+            if basic {
+                performSegueWithIdentifier("addToBasic", sender: self)
+            } else {
+                performSegueWithIdentifier("addToMain", sender: self)
+            }
+        }
+    }
+    
+    //MARK: Folder Picker delegate/dataSource methods
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int
+    {
+        return 1;
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return self.folders.count;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.folders[row];
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        self.folderTextField.text = self.folders[row];
+        self.folderTextField.endEditing(true)
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        if identifier == "backToSettings" {
+            return true
+        }
+        else {
+            if let _ = imagePicker?.image {
+                if let text = returnImageName() {
+                    //folder stuff
+                    let image = scaleDownImage(imagePicker.image!)
+                    data.add(image, label: text, folder: returnFolderName())
+                    return true
+                }
+                else {
+                    notifyUserOfError("Icon must have both an Image and a Name", popUpMessage: "Please add the name of the Image to the 'Image Name' box", popUpButtonLabel: "Okay")
+                    return false
+                }
+            }
+            else {
+                notifyUserOfError("Icon must have both an Image and a Name", popUpMessage: "Please add a photo by using the 'Take Photo' option, or by selecting an image from your device's Library", popUpButtonLabel: "Okay")
+                return false
+            }
+        }
+    }
+    
+}
---
+++ Glyph/DataModel.swift
@@ -0,0 +1,199 @@
+//
+//  DataModel.swift
+//  Glyph
+//
+//  Created by Anwar Baroudi on 6/21/15.
+//  Copyright (c) 2015 Paige Plander. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import CoreData
+
+class DataModel {
+    var iconStorage = [NSManagedObject]()
+    var imageData: [UIImage]
+    var labelData: [String]
+    var folderDict: [String: (images: [UIImage], labels: [String])]
+    let speaker: AVSpeechSynthesizer
+    var count: Int
+    let independent: Bool
+    
+    init(isNewEmptyDataModel: Bool) {
+        independent = isNewEmptyDataModel
+        folderDict = [String: (images: [UIImage], labels: [String])]()
+        speaker = AVSpeechSynthesizer()
+        count = 0
+        imageData = []
+        labelData = []
+        if independent == false {
+            iconStorage = fetchFromCoreData()
+            
+            fillDict()
+            count = iconStorage.count
+            
+        }
+    }
+    
+    func fetchFromCoreData() -> [NSManagedObject] {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "IconData")
+        var fetchResults = [NSManagedObject]?()
+        do {
+            fetchResults = try managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject]
+            
+        } catch let error {
+            print("Could not cache the response \(error)")
+            
+        }
+        let results = fetchResults as [NSManagedObject]?
+        return results!
+    }
+    
+    func isEmpty() -> Bool {
+        return iconStorage.count == 0 && imageData.count == 0
+    }
+    
+    func fillDict() {
+        for item in iconStorage {
+            let temp = item.valueForKey("folder") as! String
+            if folderDict[temp] == nil {
+                folderDict[temp] = (images: [UIImage](), labels: [String]())
+            }
+            folderDict[temp]!.labels.append(item.valueForKey("iconLabel") as! String)
+            folderDict[temp]!.images.append(UIImage(data: (item.valueForKey("iconImage") as? NSData)!)!)
+        }
+    }
+    
+    func countForFolder(folder: String) -> Int {
+        if folderDict[folder] == nil {
+            return 0
+        } else {
+            return folderDict[folder]!.labels.count
+        }
+    }
+    
+    func getImage(index: Int, folder: String) -> UIImage {
+        if independent {
+            return imageData[index]
+        }
+        return folderDict[folder]!.images[index]
+    }
+    
+    func getLabel(index: Int, folder: String) -> String {
+        if independent {
+            if labelData.isEmpty {
+                return "nil"
+            }
+            return labelData[index]
+        }
+        
+        return folderDict[folder]!.labels[index]
+        
+    }
+    
+    func add(image: UIImage, label: String, folder: String) {
+        if independent {
+            imageData.append(image)
+            labelData.append(label)
+        } else {
+            addToIconStorage(image, label: label, folder: folder)
+            
+            if folderDict[folder] == nil {
+                folderDict[folder] = (images: [UIImage](), labels: [String]())
+            }
+            folderDict[folder]!.images.append(image)
+            folderDict[folder]!.labels.append(label)
+        }
+        count += 1
+    }
+    
+    func addToIconStorage(image: UIImage, label: String, folder: String) {
+        let appDelegate =
+            UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let entity =  NSEntityDescription.entityForName("IconData",
+                                                        inManagedObjectContext:
+            managedContext)
+        let icon = NSManagedObject(entity: entity!,
+                                   insertIntoManagedObjectContext:managedContext)
+        let imageInBinaryData = UIImagePNGRepresentation(image)
+        icon.setValue(imageInBinaryData, forKey: "iconImage")
+        icon.setValue(label, forKey: "iconLabel")
+        icon.setValue(folder, forKey: "folder")
+        
+        do {
+            try managedContext.save()
+        }
+        catch let error {
+            print("Could not cache the response \(error)")
+        }
+        iconStorage.append(icon)
+    }
+    
+    // Find the Index of the Icon that we want to remove
+    func findIndex(folderName: String, labelName: String) -> Int {
+        var index = 0
+        for element in iconStorage {
+            let label = element.valueForKey("iconLabel") as! String
+            let folder = element.valueForKey("folder") as! String
+            if (label == labelName)&&(folderName == folder){
+                return index
+            }
+            else {
+                index += 1
+            }
+        }
+        
+        return index
+    }
+    
+    func remove(folderName: String, labelName: String) {
+        let index = findIndex(folderName, labelName: labelName)
+        if independent {
+            imageData.removeAtIndex(index)
+            labelData.removeAtIndex(index)
+            
+            
+        } else {
+            
+            // var folderDict: [String: (images: [UIImage], labels: [String])]
+            let tuple = folderDict[folderName]
+            let arrayOfLabels = tuple!.1
+            let labelIndex = arrayOfLabels.indexOf(labelName)
+            // fix this mess later
+            folderDict[folderName]!.0.removeAtIndex(labelIndex!)
+            folderDict[folderName]!.1.removeAtIndex(labelIndex!)
+            let appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            let context:NSManagedObjectContext = appDel.managedObjectContext
+            context.deleteObject(iconStorage[index] as NSManagedObject)
+            iconStorage.removeAtIndex(index)
+            
+            do {
+                try context.save()
+            }
+            catch let error {
+                print("Could not cache the response \(error)")
+            }
+            
+        }
+        count -= 1
+    }
+    
+    //    func removeAll() {
+    //        var i = 0
+    //        while i < count {
+    //            remove(i)
+    //            i += 1
+    //        }
+    //    }
+    
+    func speakAtIndex(index: Int, folder: String) {
+        let speech = AVSpeechUtterance(string: getLabel(index, folder: folder))
+        speech.rate = 0.5
+        speaker.speakUtterance(speech)
+    }
+    
+}
